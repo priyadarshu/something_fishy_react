b7d1860dff04c1e26ca1f0ac6736bed78cb5e1da
Priyadarshini Chettiar <45838555+priyadarshu@users.noreply.github.com>
2021-04-15 00:59:46 +0530

    Added Header and Inventory files

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
-
+import Header from "./Header";
+import Inventory from "./Inventory";
 class Application extends Component {
   render() {
     return (
-      <React.Fragment>
-        <form className="store-selector">
-          {/* This is a block comment */}
-          <h2>Please Enter a Store</h2>
-          <input type="text" required placeholder="Store Name" />
-          <button type="submit">Visit Store -></button>
-        </form>
-      </React.Fragment>
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header />
+        </div>
+        <Inventory />
+        <Inventory />
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import StorePicker from "./components/StorePicker";
 import "./css/style.css";
-
+import Application from "./components/Application";
 const rootElement = document.querySelector("#main");
 ReactDOM.render(
   <>
     <StorePicker />
+    <Application />
   </>,
   rootElement
 );
